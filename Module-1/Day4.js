c96d487bc3b6597c67fc6cf3c4572329a2064419
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-09-19 23:31:25 +0200

    Strings

---
+++ Module-1/Day4.js
@@ -0,0 +1,25 @@
+//STRINGS
+
+//String literals
+// let myNumber = 2
+// const myString = `First line
+// Second line
+// This is my value ${myNumber}`
+// console.log(myString);
+
+// const myString = 'whatever'
+// myString.slice(3,6)
+// myString.indexOf('a')
+// myString.trim()//removes white spaces
+
+const sentence = "Hello strivers we are trying the split method"
+const splitWords = sentence.split(" ")// chose to split the sentence using the space as a separator
+console.log(splitWords)
+console.log(splitWords.join(", "))//join split words with a comma
+
+console.log("asd".repeat(5))//repeats string 5 times
+
+console.log(Math.PI)//library for mathematics functions if you type 'Math'
+
+let randomNum = Math.random() //creates a random number
+console.log(randomNum)
